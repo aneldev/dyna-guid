ece728f2aaff5b83a1b75f2452d76b63627c4824
dennisat <dennis@anel.co>
2022-01-23 17:08:56 +0100

    V2

--- tests/scripts/main.test.ts
+++ tests/scripts/main.test.ts
@@ -1,22 +1,40 @@
-declare let jasmine: any, describe: any, expect: any, it: any;
-if (typeof jasmine !== 'undefined') jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
+import {
+  guid,
+  isGuid,
+} from "../../src";
 
-import {guid} from '../../src';
+describe("dyna guid", () => {
 
-describe('dyna guid test', () => {
-
-  it('should create a simple guid', () => {
+  test("Create a simple guid", () => {
     let myGUID: string = guid();
     expect(myGUID).not.toBe(undefined);
     expect(myGUID).not.toBe(null);
     expect(myGUID.length > 14).toBe(true);
   });
 
-  it('should have correct number of dashes', () => {
-    let myGUID: string = guid(7);
+  test("Correct number of dashes", () => {
+    const myGUID: string = guid(7);
     let dashes: number = 0;
-    myGUID.split('').forEach((s: string) => s === '-' ? dashes++ : null);
+    myGUID.split("").forEach((s: string) => s === "-" ? dashes++ : null);
     expect(dashes).toBe(7);
   });
 
+  test("isGuid validation with 100 random guids", () => {
+    let wrongGuid = "";
+    for (let i = 0; i < 100; i++) {
+      const createdGuid = guid();
+      if (!isGuid(createdGuid)) wrongGuid = createdGuid;
+    }
+    expect(wrongGuid).toBe("");
+  });
+
+  test("isGuid validation with space inside", () => {
+    expect(isGuid("703d76b3-1e461433-2f847b96d3ace7688")).toMatchSnapshot();
+  });
+  test("isGuid validation with wrong blocks number", () => {
+    expect(isGuid("703d76b3-2f847b96d3ace7688")).toMatchSnapshot();
+  });
+  test("isGuid validation with wrong last block", () => {
+    expect(isGuid("703d76b3-703d76b3-2f847b96d3ae7688")).toMatchSnapshot();
+  });
 });
